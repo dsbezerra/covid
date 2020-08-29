04c3a674e0e7fdc9a2a762a58207622b001e3143
Diego Bezerra <dsouzaduarte@gmail.com>
2020-08-29 03:28:18 -0300

    Better conversion

--- code/myhtml_utils.cpp
+++ code/myhtml_utils.cpp
@@ -13,35 +13,42 @@ get_attribute_by_key(myhtml_tree_node_t *node, char *key) {
     return 0;
 }
 
+size_t
+utf8_to_codepoint(u8 *data, size_t *codepoint) {
+    size_t result = 0;
+    
+    
+    if(!(data[0] & 0x80)) {      // 0xxxxxxx
+        *codepoint = data[0];
+        result = 1;
+    } 
+    else if((data[0] & 0xe0) == 0xc0) {  // 110xxxxx
+        *codepoint  = (data[0] & 0x1f) << 6;
+        *codepoint |= (data[1] & 0x3f); 
+        
+        result = 2;
+    }
+    else if((data[0] & 0xf0) == 0xe0) {  // 1110xxxx
+        *codepoint  = ((data[0] & 0x0f) << 12);
+        *codepoint |= ((data[1] & 0x3f) << 6);
+        *codepoint |= (data[2] & 0x3f);
+        
+        result = 3;
+    }
+    
+    return result;
+}
+
 u8 *
 html_utf8_text_to_extended_ascii(char *src) {
     size_t size = strlen(src);
     u8 *result = (u8 *) malloc(size + 1);
     u8 *at = result;
     while (*src) {
-        u8 c = (u8) *src;
-        if ((c & 0x80) == 0x00) {
-            *at++ = *src++;
-            continue;
-        }
-        
-        // Handle only necessary codepoints
-        if ((c & 0xE0) == 0xC0 && c == 0xC3) {
-            *src++;
-            switch ((u8) *src) {
-                case 0x93:   // Ó
-                case 0xA3:   // ç
-                case 0xA7:   // ã
-                case 0xB5: { // õ
-                    *at++ = (u32) *src + 0x40;
-                } break;
-                
-                default: {
-                    
-                } break;
-            }
-            *src++;
-        }
+        size_t codepoint;
+        size_t adv = utf8_to_codepoint((u8 *)src, &codepoint);
+        *at++ = (u8) codepoint;
+        src += adv;
     }
     
     *at = '\0';
